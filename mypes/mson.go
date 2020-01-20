3696b6745f5f37c5efd9d95157d3856244a7a1de
Mehrdad <mehrdad@otad.ir>
2020-01-20 16:41:36 +0330

    DeepGet added to MSON type

--- mypes/mson.go
+++ mypes/mson.go
@@ -1,4 +1,35 @@
 package mypes
 
+import (
+	"strings"
+)
+
 // MSON Represents a type for unstructured JSONs
 type MSON map[string]interface{}
+
+// DeepGet ...
+func (m MSON) DeepGet(path string) (value interface{}) {
+	params := strings.Split(path, ".")
+
+	lenParams := len(params)
+
+	var tmpInterface interface{}
+
+	for i := 0; i < lenParams; i++ {
+		param := params[i]
+		if i == 0 {
+			tmpInterface = m[param]
+		} else {
+			if tmpInterface == nil {
+				return "-"
+			}
+			tmpMap := tmpInterface.(MSON)
+			tmpInterface = tmpMap[param]
+		}
+
+		if i == lenParams-1 {
+			value = tmpInterface
+		}
+	}
+	return
+}
